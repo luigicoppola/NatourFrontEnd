fda07af518d7899ee452529fb7546c2085cb139a
luigicoppola1994 <94626586+luigicoppola1994@users.noreply.github.com>
2022-01-10 02:13:27 +0100

    Responsive Layout

--- app/src/main/java/com/ingsftw/natourfrontend/activities/MainActivity.kt
+++ app/src/main/java/com/ingsftw/natourfrontend/activities/MainActivity.kt
@@ -4,6 +4,7 @@ package com.ingsftw.natourfrontend.activities
 import android.annotation.SuppressLint
 import android.annotation.TargetApi
 import android.app.Dialog
+import android.content.Context
 import android.content.Intent
 import android.content.res.Configuration
 import android.os.Bundle
@@ -50,11 +51,10 @@ import twitter4j.Twitter
 import twitter4j.TwitterFactory
 import twitter4j.conf.ConfigurationBuilder
 import android.widget.RelativeLayout
+import android.view.View.MeasureSpec
 
-
-
-
-
+import android.graphics.drawable.Drawable
+import android.util.AttributeSet
 
 
 class MainActivity : AppCompatActivity() {
@@ -72,8 +72,6 @@ class MainActivity : AppCompatActivity() {
     enum class WindowSizeClass { COMPACT, MEDIUM, EXPANDED }
 
 
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -81,8 +79,6 @@ class MainActivity : AppCompatActivity() {
 
         /*val item = findViewById<View>(R.id.registrazioneLayout) as ConstraintLayout
         val schermata: View = layoutInflater.inflate(R.layout.activity_main, null)
-
-
         item.addView(object : View(this) {
             override fun onConfigurationChanged(newConfig: Configuration?) {
                 super.onConfigurationChanged(newConfig)
@@ -107,24 +103,18 @@ class MainActivity : AppCompatActivity() {
             .requestEmail()
             .build()
 
-       /* var googleButton = findViewById<ImageButton>(R.id.googleContinua)
-
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
-        googleButton.setOnClickListener {
-            signIn()
-        }
-
-        // -----------------  FINE CON GOOGLE ------------------------
-
-
-        // -------------------- ACCESSO CON TWITTER -----------------------
-        var twitterButton = findViewById<ImageButton>(R.id.twitterContinua)
-
-        twitterButton.setOnClickListener {
-            getRequestToken()
-        }
-
-*/
+        /* var googleButton = findViewById<ImageButton>(R.id.googleContinua)
+         mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+         googleButton.setOnClickListener {
+             signIn()
+         }
+         // -----------------  FINE CON GOOGLE ------------------------
+         // -------------------- ACCESSO CON TWITTER -----------------------
+         var twitterButton = findViewById<ImageButton>(R.id.twitterContinua)
+         twitterButton.setOnClickListener {
+             getRequestToken()
+         }
+ */
         // -------------------- GESTIONE LOGIN ---------------------------------
 
         var accedi = findViewById<TextView>(R.id.accediLinkText)
@@ -139,8 +129,8 @@ class MainActivity : AppCompatActivity() {
         val viewPager: ViewPager2 = findViewById(R.id.view_pager2)
 
         val fragments: ArrayList<Fragment> = arrayListOf(
-            SwipeItemUno(),
-            SwipeItemDue()
+           // SwipeItemUno(),
+            //SwipeItemDue()
         )
 
         val adapter = ViewPagerAdapter(fragments, this)
@@ -262,12 +252,7 @@ class MainActivity : AppCompatActivity() {
         //FUNZIONI GOOGLE
 
 /*
-
-
-
-
     }
-
 */
 
 
@@ -348,9 +333,8 @@ class MainActivity : AppCompatActivity() {
         }
     }}
 
-    // ------------- FUNZIONI TWITTER -------------------------
+// ------------- FUNZIONI TWITTER -------------------------
 /*lateinit var twitter: Twitter
-
     private fun getRequestToken() {
         GlobalScope.launch(Dispatchers.Default) {
             val builder = ConfigurationBuilder()
@@ -371,10 +355,8 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
-
     lateinit var twitterDialog: Dialog
     var accToken: AccessToken? = null
-
     // Show twitter login page in a dialog
     @SuppressLint("SetJavaScriptEnabled")
     fun setupTwitterWebviewDialog(url: String) {
@@ -388,7 +370,6 @@ class MainActivity : AppCompatActivity() {
         twitterDialog.setContentView(webView)
         twitterDialog.show()
     }
-
     // A client to know about WebView navigations
     // For API 21 and above
     @Suppress("OverridingDeprecatedMember")
@@ -401,7 +382,6 @@ class MainActivity : AppCompatActivity() {
             if (request?.url.toString().startsWith(TwitterConstants.CALLBACK_URL)) {
                 Log.d("Authorization URL: ", request?.url.toString())
                 handleUrl(request?.url.toString())
-
                 // Close the dialog after getting the oauth_verifier
                 if (request?.url.toString().contains(TwitterConstants.CALLBACK_URL)) {
                     twitterDialog.dismiss()
@@ -410,13 +390,11 @@ class MainActivity : AppCompatActivity() {
             }
             return false
         }
-
         // For API 19 and below
         override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
             if (url.startsWith(TwitterConstants.CALLBACK_URL)) {
                 Log.d("Authorization URL: ", url)
                 handleUrl(url)
-
                 // Close the dialog after getting the oauth_verifier
                 if (url.contains(TwitterConstants.CALLBACK_URL)) {
                     twitterDialog.dismiss()
@@ -425,7 +403,6 @@ class MainActivity : AppCompatActivity() {
             }
             return false
         }
-
         // Get the oauth_verifier
         private fun handleUrl(url: String) {
             val uri = Uri.parse(url)
@@ -436,47 +413,34 @@ class MainActivity : AppCompatActivity() {
                // getUserProfile()
             }
         }
-
         suspend fun getUserProfile() {
             val usr = withContext(Dispatchers.IO) { twitter.verifyCredentials() }
-
             //Twitter Id
             val twitterId = usr.id.toString()
             Log.d("Twitter Id: ", twitterId)
-
             //Twitter Handle
             val twitterHandle = usr.screenName
             Log.d("Twitter Handle: ", twitterHandle)
-
             //Twitter Name
             val twitterName = usr.name
             Log.d("Twitter Name: ", twitterName)
-
             //Twitter Email
             val twitterEmail = usr.email
             Log.d("Twitter Email: ", twitterEmail ?: "'Request email address from users' on the Twitter dashboard is disabled")
-
             // Twitter Profile Pic URL
             val twitterProfilePic = usr.profileImageURLHttps.replace("_normal", "")
             Log.d("Twitter Profile URL: ", twitterProfilePic)
-
             // Twitter Access Token
             Log.d("Twitter Access Token", accToken?.token ?: "")
-
         }
     }
-
-
     // --------------------------- FINE ON CREATE ---------------------------
-
     private fun signIn() {
         val signInIntent = mGoogleSignInClient.signInIntent
         startActivityForResult(
             signInIntent, RC_SIGN_IN
         )
     }
-
-
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == RC_SIGN_IN) {
@@ -485,7 +449,6 @@ class MainActivity : AppCompatActivity() {
             handleSignInResult(task)
         }
     }
-
     private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
         try {
             val account = completedTask.getResult(
@@ -494,27 +457,17 @@ class MainActivity : AppCompatActivity() {
             // Signed in successfully
             val googleId = account?.id ?: ""
             Log.i("Google ID", googleId)
-
             val googleFirstName = account?.givenName ?: ""
             Log.i("Google First Name", googleFirstName)
-
             val googleLastName = account?.familyName ?: ""
             Log.i("Google Last Name", googleLastName)
-
             val googleEmail = account?.email ?: ""
             Log.i("Google Email", googleEmail)
-
             val googleProfilePicURL = account?.photoUrl.toString()
             Log.i("Google Profile Pic URL", googleProfilePicURL)
-
             val googleIdToken = account?.idToken ?: ""
             Log.i("Google ID Token", googleIdToken)
-
-
-
             // createUser(googleEmail,googleIdToken,googleFirstName+" "+googleLastName,"")
-
-
         } catch (e: ApiException) {
             // Sign in was unsuccessful
             Log.e(
@@ -522,16 +475,12 @@ class MainActivity : AppCompatActivity() {
             )
         }
     }
-
-
     private fun signOut() {
         mGoogleSignInClient.signOut()
             .addOnCompleteListener(this) {
                 // Update your UI here
             }
     }
-
-
     private fun revokeAccess() {
         mGoogleSignInClient.revokeAccess()
             .addOnCompleteListener(this) {
@@ -539,66 +488,31 @@ class MainActivity : AppCompatActivity() {
             }
     }
 }//--------------- FINE
-
-
-
-
-
-
 */
 
 
 /*
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
-
-
         val viewPager: ViewPager2 = findViewById(R.id.view_pager2)
-
         val fragments: ArrayList<Fragment> = arrayListOf(
             ItemUno(),
             tre()
-
-
         )
-
-
-
-
         val adapter = ViewPagerAdapter(fragments, this)
-
         viewPager.adapter = adapter
-
-
         val indicator = findViewById<CircleIndicator3>(R.id.indicator)
-
         val registrazioneButton = findViewById(R.id.accedi_button) as Button
-
-
         val email = findViewById<EditText>(R.id.emailText)
         val passw = findViewById<EditText>(R.id.passwordText)
         val nomeCompleto = findViewById<EditText>(R.id.nomeText)
         val dataNascita = findViewById<EditText>(R.id.Date)
-
-
         var continua = findViewById<Button>(R.id.accedi_button)
-
-
         if (viewPager.currentItem == 0)
             continua.setText("Continua")
         else
             continua.setText("Registrati")
-
-
-
-
-
-
         registrazioneButton.setOnClickListener {
-
-
-
         if (viewPager.currentItem == 0) {
-
             viewPager.setCurrentItem(1, true)
         } else
             createUser(
@@ -607,36 +521,20 @@ class MainActivity : AppCompatActivity() {
                 nomeCompleto.text.toString(),
                 dataNascita.text.toString()
             )
-
-
     }
-
-
-
-
-
-
-
     viewPager?.registerOnPageChangeCallback(
     object : ViewPager2.OnPageChangeCallback() {
         override fun onPageSelected(position: Int) {
             super.onPageSelected(position)
             if (position == 0) {
-
                 continua.setText("Continua")
             } else {
-
                 continua.setText("Registrati")
-
             }
         }
-
         override fun onPageScrollStateChanged(state: Int) {
             super.onPageScrollStateChanged(state)
-
-
         }
-
         override fun onPageScrolled(
             position: Int,
             positionOffset: Float,
@@ -645,53 +543,30 @@ class MainActivity : AppCompatActivity() {
             super.onPageScrolled(position, positionOffset, positionOffsetPixels)
         }
     })
-
-
-
-
-
-
-
-
-
-
     }
-
 }
-
     private fun createUser(email: String, passw: String, nomeCompleto : String, dataNascita: String) {
-
         // Create Retrofit
         val retrofit = Retrofit.Builder()
             .baseUrl("http://10.0.2.2:8080/")
             .build()
-
         // Create Service
         val service = retrofit.create(RestApi::class.java)
-
-
-
-
         // Create JSON using JSONObject
         val jsonObject = JSONObject()
         jsonObject.put("email",email)
         jsonObject.put("password", passw)
         jsonObject.put("nomeCompleto", nomeCompleto)
         jsonObject.put("dataNascita", dataNascita)
-
         // Convert JSONObject to String
         val jsonObjectString = jsonObject.toString()
-
         // Create RequestBody ( We're not using any converter, like GsonConverter, MoshiConverter e.t.c, that's why we use RequestBody )
         val requestBody = jsonObjectString.toRequestBody("application/json".toMediaTypeOrNull())
-
         CoroutineScope(Dispatchers.IO).launch {
             // Do the POST request and get response
             val response = service.createUser(requestBody)
-
             withContext(Dispatchers.Main) {
                 if (response.isSuccessful) {
-
                     // Convert raw JSON to pretty JSON using GSON library
                     val gson = GsonBuilder().setPrettyPrinting().create()
                     val prettyJson = gson.toJson(
@@ -701,55 +576,34 @@ class MainActivity : AppCompatActivity() {
                         )
                     )
                     Log.d("Pretty Printed JSON :", prettyJson)
-
-
                 } else {
                     Log.e("RETROFIT_ERROR", response.code().toString())
-
                 }
             }
         }
     }
-
-
-
 */
 
 
 
 /*
         val registrazioneButton = findViewById(R.id.registrazioneButton) as Button
-
         registrazioneButton.setOnClickListener {
-
             viewPager.apply {
                 beginFakeDrag()
                 fakeDragBy(-10f)
                 endFakeDrag()
-
             }
-
-
         }
         val continua = findViewById(R.id.bottone) as Button
         var email = findViewById<EditText>(R.id.email)
         var psw = findViewById<EditText>(R.id.pass)
-
-
-
         continua.setOnClickListener{
             psw.setVisibility(View.VISIBLE)
             val layoutParams = continua.layoutParams as RelativeLayout.LayoutParams
             layoutParams.setMargins(520, 300, 520, 100)
             continua.layoutParams = layoutParams
-
             }
-
-
-
-
-
-
 */
 
 
@@ -775,155 +629,72 @@ class MainActivity : AppCompatActivity() {
 
 
 /*
-
-
-
 class MainActivity : AppCompatActivity() {
-
-
     private val client = OkHttpClient()
-
-
     private var titleList = mutableListOf<String>()
     private var descList = mutableListOf<String>()
     private var imageList = mutableListOf<Int>()
-
-
-
     private var titleList = mutableListOf<String>()
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registrazione)
-
-
-
         //postToList()
         val viewPager: ViewPager2 = findViewById(R.id.view_pager2)
-
-
         viewPager.adapter = ViewPagerAdapter(titleList, descList, imageList)
         viewPager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
-
-
-
      viewPager.adapter = ViewPagerAdapter(titleList)
         viewPager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
-
-
-
-
-
-
         val fragments: ArrayList<Fragment> = arrayListOf(
            ItemUno(),
            fragment_item_due()
-
-
         )
-
         val adapter = ViewPagerAdapter(fragments,this)
         viewPager.adapter = adapter
-
-
-
-
-
         val indicator = findViewById<CircleIndicator3>(R.id.indicator)
         indicator.setViewPager(viewPager)
-
         val continua = findViewById(R.id.continua_button) as Button
-
         continua.setOnClickListener{
-
             viewPager.apply{
                 beginFakeDrag()
                 fakeDragBy(-10f)
                 endFakeDrag()
-
         }
-
-
         }
-
     }
-
-
-
     private fun addToList(title: String) {
         titleList.add(title)
     }
-
-
-
     private fun postToList() {
         for (i in 1..3) {
-
             addToList("Titolo $i")
-
         }
     }
-
-
-
-
-
-
-
-
-
-
-
     private fun addToList(title: String, description: String, image: Int) {
         titleList.add(title)
         descList.add(description)
         imageList.add(image)
     }
-
     private fun postToList() {
         for (i in 1..3) {
-
             addToList("Titolo $i", "Descrizione $i", R.drawable.email_section)
-
         }
     }
-
-
-
 }
-
-
        val email = findViewById(R.id.email) as EditText
         val continua = findViewById(R.id.continua) as Button
-
         val layoutuno = findViewById<View>(R.id.Uno) as RelativeLayout
         val layoutdue = findViewById<View>(R.id.Due) as RelativeLayout
         layoutdue.setVisibility(View.GONE)
-
-
-
         //val password = findViewById<View>(R.id.psw) as TextView
         //password.setVisibility(View.GONE);
-
-
-
-
         continua.setOnClickListener {
             layoutuno.setVisibility(View.INVISIBLE)
             layoutdue.setVisibility(View.VISIBLE)
-
         }
-
-
-
-
     fun register(view: View){
         run("http://10.0.2.2:8080/utente/all")
     }
-
 fun run(url: String){
-
     var testo= findViewById(R.id.editTextTextEmailAddress2) as TextView
     val request = Request.Builder().url(url).build()
     client.newCall(request).enqueue(object : Callback{
@@ -934,7 +705,5 @@ fun run(url: String){
         override fun onResponse(call: Call, response: Response) =
             testo.setText(response.body()?.string())
     })
-
 }
 */
-
